7088e94c9aa07a601ba44f2c097f87866d4fc9a9
andong.lad <andong.lad@alibaba-inc.com>
2015-09-13 18:12:58 +0800

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+npmdiy - lowercase
+========================
+
+A demo package of lowercase
+
+```{bash}
+npm install npmdiy
+```
+
+## author
+
+yiqiao
+
+## License
+
+MIT
---
+++ projectInit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir bin
+touch bin/lowercase
+touch bin/lowercase.bat
+mkdir test
+mkdir test/data
+touch test/data/sample.txt
+touch lowercase.js
+touch example.js
+touch README.md
